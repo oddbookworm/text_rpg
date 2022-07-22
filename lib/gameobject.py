fb83cf215906863753e916451035782c41952851
oddbookworm <49015102+oddbookworm@users.noreply.github.com>
2022-07-22 14:21:35 -0500

    finished base functionality

--- lib/gameobject.py
+++ lib/gameobject.py
@@ -1,7 +1,13 @@
 import pygame
 
 class GameObject(pygame.sprite.Sprite):
-    def __init__(self, image: pygame.Surface, pos: pygame.Vector2):
+    def __init__(self, image: pygame.Surface, pos: pygame.Vector2) -> None:
         super().__init__()
         self.img = image
         self.rect = image.get_rect(topleft = pos)
+    
+    def draw(self, screen: pygame.Surface) -> None:
+        screen.blit(self.img, self.rect)
+        
+    def update(self, offset: pygame.Vector2) -> None:
+        self.rect.move(offset)
